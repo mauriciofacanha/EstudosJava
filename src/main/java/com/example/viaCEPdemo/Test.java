6dd9cba58494e68cffbc9dfb374b61f91acdfb7f
Mauricio Fcnh <mauriciofacanha05@gmail.com>
2026-02-10 23:55:14 -0300

    adicionando a classe Test

---
+++ src/main/java/com/example/viaCEPdemo/Test.java
@@ -0,0 +1,10 @@
+package com.example.viaCEPdemo;
+
+public class Test {
+
+    private String nome;
+
+    public void mensagem (String nome1){
+        System.out.println("Nome: " + nome);
+    }
+}
